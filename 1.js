c2ca80aa3588beabee3722aaa3d6d71e40909fb8
artibhagat19 <artibhagat23@navgurukul.org>
2024-09-24 13:47:28 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,74 @@
+// let a = 1;
+// count = 0;
+// for(i = 1; i <= a;i++){
+//     if(a % i == 0){
+//         count++;
+//     }
+    
+// }
+// if(count == 2){
+//     console.log("prime");
+// }else{
+//     console.log("not prime");
+// }
+
+// let a = 10;
+// for(let i = 1; i <= a; i++){
+//     let count = 0;
+//     for(let j = 1; j <= i; j++){
+//         if(i % j == 0){
+//             count++;
+//         }
+//     }
+//     if(count == 2){
+//         console.log(i);
+//     }else{
+
+//     }
+// }
+
+// let a = 5;
+// let i = 0;
+
+// while (i < a) {
+//     console.log(i); 
+//     i--; 
+// }
+
+// let j = 5;
+
+// while (j > 0) {
+//     console.log(j); 
+//     j--; 
+// }
+
+// const input = require('readline-sync');
+// let j = input.question('')
+//  while (j >= 0) {
+//     console.log(j); 
+//     j--; 
+// }
+
+// }else{
+//     console.log("not prime");
+// }
+
+
+// const input = require('readline-sync');
+//  let a = input.question('enter number')
+// for(let i = 2; i <= a; i++){
+//     console.log(i);
+
+// }
+
+
+const input = require('readline-sync');
+let a = input.question('enter number')
+
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= a);
+
+   
